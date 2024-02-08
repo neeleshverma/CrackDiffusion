7439b1562d1d44eabb3ad506847dfa1933bc5487
Neelesh Verma <neeleshverma13@gmail.com>
2024-02-08 18:09:55 -0500

    npy converter

--- scripts/ddpm/npy_converter.py
+++ scripts/ddpm/npy_converter.py
@@ -6,23 +6,26 @@ from tqdm import tqdm
 from PIL import Image
 
 # Ground Truth, Original and Destination Folder
-gt_folder = "/home/neelesh/crackdiff/datasets/cracktree200/gt"
-orig_folder = "/home/neelesh/crackdiff/datasets/cracktree200/orig"
-dest_folder = "/home/neelesh/crackdiff/datasets/cracktree200/test"
+gt_folder = "/home/neelesh/crackdiff/datasets/cfd/gt"
+orig_folder = "/home/neelesh/crackdiff/datasets/cfd/orig"
+dest_folder = "/home/neelesh/crackdiff/datasets/cfd/test"
 
-convert_to_npy = False
+convert_to_npy = True
 
+if convert_to_npy:
+    folder = gt_folder
+else:
+    folder = orig_folder
 files = os.listdir(folder)
 
 # Resizing, converting to npy (if required) and moving to dest folder
 for i in tqdm(range(len(files))):
     filename = os.path.join(folder, files[i])
-
-    img = Image.open(filename)
-    resized_img = img.resize((256, 256), Image.ANTIALIAS)
+    img = cv2.imread(filename)
+    resized_img = cv2.resize(img, (256, 256), interpolation = cv2.INTER_LINEAR)
 
     if convert_to_npy:
-        resized_img = np.array(resized_img)
+        resized_img[resized_img >= 1] = 255
         output_filename = os.path.splitext(files[i])[0] + '.npy'
         output_path = os.path.join(dest_folder, output_filename)
         np.save(output_path, resized_img)
@@ -30,4 +33,4 @@ for i in tqdm(range(len(files))):
     else:
         output_filename = files[i]
         output_path = os.path.join(dest_folder, output_filename)
-        resized_img.save(output_path)
+        cv2.imwrite(output_path, resized_img)
